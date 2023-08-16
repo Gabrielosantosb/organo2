0d53de066528654d7acbc23d3f7c27c3becf0345
gabrielosantosb <gabrielosantosb@gmail.com>
2023-08-16 17:41:45 -0300

    alteracoes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 import { Forms } from "./components/form";
 import { Header } from "./components/header";
 
 function App() {
+  const [employee, setEmployee] = useState([]);
+  const groups = [
+    {
+      nome: "Garçom",
+      corPrimaria: "#57C278",
+      color: "#D9F7E9",
+    },
+    {
+      nome: "Cozinha",
+      corPrimaria: "#82CFFA",
+      color: "#E8F8FF",
+    },
+    {
+      nome: "Limpeza",
+      corPrimaria: "#A6D157",
+      color: "#F0F8E2",
+    },
+    {
+      nome: "Gerência",
+      corPrimaria: "#E06B69",
+      color: "#FDE7E8",
+    },
+    {
+      nome: "Recepção",
+      corPrimaria: "#DB6EBF",
+      color: "#FAE9F5",
+    },
+  ];
+
   return (
     <>
       <Header />
-      <Forms/>
+      <Forms groups={groups} />
     </>
   );
 }
